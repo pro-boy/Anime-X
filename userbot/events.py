6625718d2ec33d52718634fb5025e31108aced22
PROBOY <71318599+pro-boy@users.noreply.github.com>
2021-02-09 11:54:33 +0530

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -86,7 +86,7 @@ def register(**args):
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
                     text = "**USERBOT ERROR REPORT**\n"
-                    link = "[DARK COBRA Support Chat](https://t.me/Dark_cobra_support)"
+                    link = "[ANIME-X Support Chat](https://t.me/AnimeXSupportChat)"
                     text += "If you want to, you can report it"
                     text += f"- just forward this message to {link}.\n"
                     text += "Nothing is logged except the fact of error and date\n"
